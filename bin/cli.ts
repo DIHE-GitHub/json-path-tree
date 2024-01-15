44bb5b48bd7fffadcf7d87452f85a38354644940
limao <limao@qunhemail.com>
2024-01-15 15:42:50 +0800

    🐞 fix: 路径解析

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,7 +1,11 @@
 import { readFile, writeFile } from "fs/promises";
-import { argv } from "zx";
+import { argv, path } from "zx";
 import { getPathTree } from "../src/utils/json-path-tree";
 
-const jsonStr = await readFile(argv.i, 'utf-8')
-const pathTree = getPathTree(JSON.parse(jsonStr))
-await writeFile(argv.o, JSON.stringify(pathTree, null, 2))
+const inputPath = path.resolve(argv.i)
+const outputPath = path.resolve(argv.o)
+const jsonStr = await readFile(inputPath, 'utf-8')
+const json = JSON.parse(jsonStr)
+const pathTree = getPathTree(json)
+const pathTreeStr = JSON.stringify(pathTree, null, 2)
+await writeFile(outputPath, pathTreeStr)
